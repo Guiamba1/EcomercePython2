32486a0f97fb997182ae8fa0e1fcf46a371078c6
Oscar Alberto Guiamba <oscarguiamba.a@gmail.com>
2021-09-27 14:17:19 -0700

    hora
    alteracao da hora dosistema

--- lojaPyton/settings.py
+++ lojaPyton/settings.py
@@ -75,8 +75,19 @@ WSGI_APPLICATION = 'lojaPyton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': 'db',
+
+        'PORT': '5432',
+
     }
 }
 
@@ -105,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'ZW'
 
 USE_I18N = True
 
